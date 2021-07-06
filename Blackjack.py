644cc35826724efd498d409531a1108d27b92a0f
JeremiahLiao <jeremiahjliao@gmail.com>
2021-07-06 10:34:26 -0700

    Started User Gameplay coding

--- Blackjack.py
+++ Blackjack.py
@@ -1,8 +1,10 @@
+import random
+
 class Deck:
 	ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 	suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 	values = [1,2,3,4,5,6,7,8,9,10,10,10,10]
-	deck = {}
+	deck = []
 
 	def __init__(self):
 		deck_key = 1
@@ -10,12 +12,52 @@ class Deck:
 		for rank in self.ranks:
 			for suit in self.suits:
 				card_name = rank + " of " + suit
-				# self.deck.append([card_name,self.values[values_index]])
-				self.deck[deck_key] = [card_name, self.values[values_index]]
+				self.deck.append([card_name,self.values[values_index]])
 				deck_key += 1
-
 			values_index += 1
+		random.shuffle(self.deck)
+	
+	def deal(self):
+		card = self.deck.pop(0)
+		random.shuffle(self.deck)
+		return card
 
-		print(self.deck)
+	def make_play(self):
+		return self.deal()
 
-Deck()
+	def printCards(self, cards):
+		statement = "Your cards are the"
+		for card in cards:
+			statement += " " + card + "," 
+		return statement
+
+
+start = input("Welcome! Type \"Start\" to start! : ")
+game = Deck()
+player_cards = []
+player_value = 0
+dealer_cards = []
+dealer_value = 0
+
+if(start == "Start"):
+	for i in range(2):
+		dealer_card = game.deal()
+		dealer_cards.append(dealer_card[0])
+		dealer_value += dealer_card[1]
+
+		player_card = game.deal()
+		player_cards.append(player_card[0])
+		player_value += player_card[1]
+
+	print(game.printCards(player_cards))
+	print(player_value)
+
+	action = input("Would you like to Hit (H) or Stand (S)? : ")
+	if(action == "H"):
+		card = game.make_play()
+		player_cards.append(card[0])
+		player_value += card[1]
+		
+	print(player_cards)
+
+
